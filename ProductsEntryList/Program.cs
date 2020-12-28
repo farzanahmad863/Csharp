3040a8727f0f8a863a0f5c408a1c564e21564812
Farzan Ahmad <44917528+farzanahmad863@users.noreply.github.com>
2020-12-28 16:23:03 +0100

    Farzan patch (#2)
    * Created project and files
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md

---
+++ ProductsEntryList/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> ProductsList = new List<Product>();
+            bool exit = false;
+            while (!exit)
+            {
+                Console.WriteLine("Please enter the product Name!");
+                string productName = Console.ReadLine();
+                if (productName.ToLower() == "q")
+                {
+                    exit = true;
+                    List<Product> SortedList = ProductsList.OrderBy(item => item.Price).ToList();
+                    int sumOfPrices = 0;
+                    Console.WriteLine("Name".PadRight(20) + "Category".PadRight(20) + "Price");
+                    foreach (var product in SortedList)
+                    {
+                        Console.WriteLine(product.Name.PadRight(20) + product.Category.PadRight(20) + product.Price);
+                        sumOfPrices += Int32.Parse(product.Price);
+                    }
+                    Console.WriteLine("sum:".PadRight(40) + sumOfPrices);
+                    break;
+                }
+                Console.WriteLine("Please enter the product Category!");
+                string productCategory = Console.ReadLine();
+                Console.WriteLine("Please enter the product Price!");
+                string productPrice = Console.ReadLine();
+                Product userProduct = new Product(); //Created object of product
+                userProduct.AddProductDetails(productName, productCategory, productPrice); //replaced user given values in object
+                ProductsList.Add(userProduct); //added the object to the list }
+            }
+        }
+        class Product
+        {
+            public string Name { get; set; }
+            public string Category { get; set; }
+            public string Price { get; set; }
+            public void AddProductDetails(string name, string category, string price)
+            {
+                Name = name;
+                Category = category;
+                Price = price;
+            }
+        }
+    }
+}
+
--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # Csharp
+## Intro
+Write a console app which starts by asking:
+1. Category
+2. Product Name
+3. Price
+
+## Level1
+- [x] Create classes for Category and Product. Use these classes when you add items to a list.
+- [x] You
+should be able to add items to the list(s) until you write "q" (for quit).
+- [x] Exit by presenting the list.
+
+## Level2
+- [x] Add price and summarize inputs when presenting.
+- [x] Quit the list after "q" is written.
+- [x] The list should be sorted from low price to high and a sum at the bottom.
+## Level3
+- [x] Refactor your code using "Linq".
+- [x] Add Error handling to your console app.
+
  
